7c624929104549d4707706e495f4090af4a9edd5
Andre Murphy <andremurphy98@outlook.com>
2020-11-08 18:59:54 -0700

    Final Project

--- fa/nfa/NFA.java
+++ fa/nfa/NFA.java
@@ -19,8 +19,6 @@ public class NFA implements NFAInterface{
 
 		// The alphabet of this DFA
 		Set<Character> alphabet;
-
-		Set<Character> transitions;
 		
 		// Set of all the final states of this DFA.
 		Set<NFAState> finalStates;
@@ -31,10 +29,9 @@ public class NFA implements NFAInterface{
 		public NFA() {
 			states = new HashSet<NFAState>();
 			alphabet = new HashSet<>();
-			transitions = new HashSet<>();
 			finalStates = new HashSet<NFAState>();
 		}
-		//think this works? need to test but logic seems correct...
+	//add transition and 'link' two states
 	public void addTransition(String fromState, char onSymb, String toState) {
 		for (NFAState s : states) { //iterate through states
 			if (fromState.equals(s.getName())) { //if equal...
@@ -142,42 +139,42 @@ public class NFA implements NFAInterface{
 	@Override
 	//BFS algo for returning a NFA from NFA
 	public DFA getDFA() {
-		DFA dfa = new DFA(); 									//declare dfa
-		Queue<Set<NFAState>> queue = new LinkedList<>(); 		//used a linked list as the start for adding the initial state
-		Set<String> tracker = new HashSet<>();		 			//hash set to keep track of states visited
-		Set<NFAState> currState = eClosure(initial); 			//find the current state
-		queue.add(currState); 									//add current state to queue
-		tracker.add(currState.toString());						//add current state to tracker set			
-		dfa.addStartState(currState.toString()); 				//set the final state for the DFA
-		while(!queue.isEmpty()){ 								//while there are still states...
-			currState = queue.remove(); 						//dequeue the last state
-			for(Character sigma: alphabet){ 					//for everything in the language...
-				Set<NFAState> tranStates = new HashSet<>(); 	//create a set for transition states
-				if(sigma != epsilon){ 							//if is not epsilon
-					for(NFAState s: currState){ 				//for all states in our current states
+		DFA dfa = new DFA(); //declare dfa
+		Queue<Set<NFAState>> queue = new LinkedList<>(); //used a linked list as the start for adding the initial state
+		Set<String> tracker = new HashSet<>();//hash set to keep track of states visited
+		Set<NFAState> currState = eClosure(initial); //find the current state
+		queue.add(currState); //add current state to queue
+		tracker.add(currState.toString());	//add current state to tracker set			
+		dfa.addStartState(currState.toString()); //set the final state for the DFA
+		while(!queue.isEmpty()){ //while there are still states...
+			currState = queue.remove(); //dequeue the last state
+			for(Character sigma: alphabet){ //for everything in the language...
+				Set<NFAState> tranStates = new HashSet<>();//create a set for transition states
+				if(sigma != epsilon){ 	//if is not epsilon
+					for(NFAState s: currState){ //for all states in our current states
 						Set<NFAState> tran = getToState(s,sigma); //make a set of transition states
-						if(tran != null){ 						//if the transition exits...
+						if(tran != null){ //if the transition exits...
 							tranStates.addAll(tran); 
 						}
 					}
-					for(NFAState s:tranStates){ 					//this iterates through all transition states
-						tranStates.addAll(eClosure(s)); 			//add epsilon transition states recursively
+					for(NFAState s:tranStates){ //this iterates through all transition states
+						tranStates.addAll(eClosure(s)); //add epsilon transition states recursively
 					}
-					if(!tracker.contains(tranStates.toString())){ 	//if we still havent gone through all states...
+					if(!tracker.contains(tranStates.toString())){ //if we still havent gone through all states...
 						boolean isFinal = false; 
-						for(NFAState s : tranStates){ 				//iterate through transition states and check for a final state
+						for(NFAState s : tranStates){ //iterate through transition states and check for a final state
 							if(s.isFinal()){
-								isFinal = true; 					//set boolean if found
+								isFinal = true; //set boolean if found
 							}
 						}
-						if(isFinal){ 								//add the found final states to the DFA
+						if(isFinal){ //add the found final states to the DFA
 							dfa.addFinalState(tranStates.toString());
 						}
-						else{ 										//else it was a normal state, add to the DFA
+						else{ //else it was a normal state, add to the DFA
 							dfa.addState(tranStates.toString());
 						}
-						queue.add(tranStates); 						//add transition states to the queue to deal with them on next loop
-						tracker.add(tranStates.toString()); 		//add transitions states to already traveled set
+						queue.add(tranStates); 	//add transition states to the queue to deal with them on next loop
+						tracker.add(tranStates.toString()); //add transitions states to already traveled set
 					}
 					dfa.addTransition(currState.toString(),sigma,tranStates.toString()); //add DFA transition
 				}	
